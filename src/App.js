c8959bc1daf934a1d780747e2e2aeaf024ddb983
Sam De Maeyer <sam_demaeyer@hotmail.com>
2020-04-11 12:03:37 +0100

    Use HashRouter

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import Board from './components/Board';
 import SpyMaster from './components/SpyMaster';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <Router>
+    <HashRouter>
       <Route exact path="/" component={Board} />
       <Route exact path="/spy-master/:spyId" component={SpyMaster} />
-    </Router>
+    </HashRouter>
   );
 }
 
