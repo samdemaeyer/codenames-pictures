27d7e7f191834dc0c59e9ec8ca99d57e6758d4bd
Sam De Maeyer <sam_demaeyer@hotmail.com>
2020-04-11 09:39:57 +0100

    Updates

---
+++ precache-manifest.88284c4dbddcc879558698e24af7631a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51394e6f1a7794e259caa744c5b0e8f9",
+    "url": "/codenames-pictures/index.html"
+  },
+  {
+    "revision": "49cfe6caf9248eea5ed3",
+    "url": "/codenames-pictures/static/css/main.7e9f8693.chunk.css"
+  },
+  {
+    "revision": "de67e547a2729624a41e",
+    "url": "/codenames-pictures/static/js/2.c57cd416.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/codenames-pictures/static/js/2.c57cd416.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49cfe6caf9248eea5ed3",
+    "url": "/codenames-pictures/static/js/main.1b921efc.chunk.js"
+  },
+  {
+    "revision": "6d5b1cd8451d3242cb22",
+    "url": "/codenames-pictures/static/js/runtime-main.a97905eb.js"
+  }
+]);
